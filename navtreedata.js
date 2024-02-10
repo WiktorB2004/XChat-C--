0c06d2d3fcac5c953ede3af4720f6c1301dd7f80
WiktorB2004 <WiktorB2004@users.noreply.github.com>
2024-02-10 04:07:09 +0000

    deploy: c6eca09e3b4878023952b6fc40e1c8a64a56f43c

--- navtreedata.js
+++ navtreedata.js
@@ -26,7 +26,11 @@ var NAVTREE =
 [
   [ "XChat", "index.html", [
     [ "Files", "files.html", [
-      [ "File List", "files.html", "files_dup" ]
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ]
+      ] ]
     ] ]
   ] ]
 ];
--- navtreeindex0.js
+++ navtreeindex0.js
@@ -1,12 +1,16 @@
 var NAVTREEINDEX0 =
 {
 "client_2src_2main_8cpp.html":[0,0,0,0,0],
+"client_2src_2main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4":[0,0,0,0,0,0],
 "dir_41e1742e44e2de38b3bc91f993fed282.html":[0,0,1],
 "dir_a063914ab482288b7b0e1b2fca86158c.html":[0,0,1,0],
 "dir_b565d458ba0ce2faccd0ea2cd60c04eb.html":[0,0,0,0],
 "dir_db3a54907829b36871118d03417739cd.html":[0,0,0],
 "files.html":[0,0],
+"globals.html":[0,1,0],
+"globals_func.html":[0,1,1],
 "index.html":[],
 "pages.html":[],
-"server_2src_2main_8cpp.html":[0,0,1,0,0]
+"server_2src_2main_8cpp.html":[0,0,1,0,0],
+"server_2src_2main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4":[0,0,1,0,0,0]
 };
