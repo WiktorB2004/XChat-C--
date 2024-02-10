5912e66f702d4cd50dab670571de2b92732dc310
WiktorB2004 <WiktorB2004@users.noreply.github.com>
2024-02-10 03:39:37 +0000

    deploy: edd48c3e474ab41356f2296c5be0a8fd88a58760

---
+++ dir_b565d458ba0ce2faccd0ea2cd60c04eb.js
@@ -0,0 +1,4 @@
+var dir_b565d458ba0ce2faccd0ea2cd60c04eb =
+[
+    [ "main.cpp", "client_2src_2main_8cpp.html", null ]
+];
---
+++ navtreeindex0.js
@@ -0,0 +1,12 @@
+var NAVTREEINDEX0 =
+{
+"client_2src_2main_8cpp.html":[0,0,0,0,0],
+"dir_41e1742e44e2de38b3bc91f993fed282.html":[0,0,1],
+"dir_a063914ab482288b7b0e1b2fca86158c.html":[0,0,1,0],
+"dir_b565d458ba0ce2faccd0ea2cd60c04eb.html":[0,0,0,0],
+"dir_db3a54907829b36871118d03417739cd.html":[0,0,0],
+"files.html":[0,0],
+"index.html":[],
+"pages.html":[],
+"server_2src_2main_8cpp.html":[0,0,1,0,0]
+};
