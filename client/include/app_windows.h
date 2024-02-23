af0ad041fe705b672949fa5d1588e70bc09aa98e
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2024-02-23 20:59:26 +0100

    Created documentation for windows and chat connection @WiktorB2004

--- client/include/app_windows.h
+++ client/include/app_windows.h
@@ -17,53 +17,174 @@
 #include <QJsonObject>
 #include "message.h"
 
+/**
+ * @brief The LoginWindow class
+ *
+ * This class represents the login window of the application.
+ */
 class LoginWindow : public QMainWindow
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Construct a new Login Window object
+     *
+     * @param parent The parent widget.
+     */
     explicit LoginWindow(QWidget *parent = nullptr);
+
+    /**
+     * @brief Destroy the Login Window object
+     *
+     */
     ~LoginWindow();
+
+    /**
+     * @brief The data of each user input in JSON format
+     *
+     * It stores values of username, password and serever ip.
+     */
     QJsonDocument m_inputData;
 
 signals:
+    /**
+     * @brief Signal emitted when login is successful.
+     *
+     */
     void loginSuccess();
+
+    /**
+     * @brief Signal emitted when login is unsuccessful.
+     *
+     */
     void loginFailure();
 
 private slots:
+    /**
+     * @brief Slot to update the input data after any change.
+     *
+     */
     void updateInputData();
+
+    /**
+     * @brief Slot to handle login button submission.
+     *
+     */
     void loginSubmit();
 
 private:
+    /**
+     * @brief Line edit for server IP.
+     *
+     */
     QLineEdit *serverIpLineEdit;
+
+    /**
+     * @brief Line edit for username.
+     *
+     */
     QLineEdit *usernameLineEdit;
+
+    /**
+     * @brief Line edit for password.
+     *
+     */
     QLineEdit *passwordLineEdit;
+
+    /**
+     * @brief Submit button for login.
+     *
+     */
     QPushButton *submitButton;
 };
 
+/**
+ * @brief The ChatWindow class
+ *
+ * This class represents the chat window of the application.
+ */
 class ChatWindow : public QMainWindow
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Construct a new Chat Window object
+     *
+     * @param client_username The username of the client.
+     * @param parent The parent widget.
+     */
     explicit ChatWindow(QString client_username, QWidget *parent = nullptr);
+
+    /**
+     * @brief Destroy the Chat Window object
+     *
+     */
     ~ChatWindow();
+
+    /**
+     * @brief The value of line edits inside chat window.
+     *
+     */
     QJsonObject m_inputData;
-    void updateMessageList();
+
+    /**
+     * @brief The list of messages.
+     *
+     */
     std::vector<Message> message_list;
 
-
+    /**
+     * @brief Refresh the message list.
+     *
+     */
+    void updateMessageList();
 signals:
+    /**
+     * @brief Signal to send a message.
+     *
+     * @param msg The message to send.
+     */
     void sendMessage(QJsonObject msg);
 
 public slots:
+    /**
+     * @brief Slot to handle received messages.
+     *
+     * @param msg The received message.
+     */
     void handleMessageRecieve(Message msg);
 
 private slots:
+    /**
+     * @brief Slot to update the input data.
+     *
+     */
     void updateInputData();
+
+    /**
+     * @brief Slot to handle sending messages.
+     *
+     */
     void handleMessageSend();
 
 private:
+    /**
+     * @brief Line edit for message input.
+     *
+     */
     QLineEdit *messageLineEdit;
+
+    /**
+     * @brief Layout for the message list.
+     *
+     */
     QVBoxLayout *messageListVBoxLayout;
+
+    /**
+     * @brief The username of the client.
+     *
+     */
+
     QString client_username;
 };
 
--- client/include/chat_connection.h
+++ client/include/chat_connection.h
@@ -5,31 +5,86 @@
 #include <QtWebSockets>
 #include "message.h"
 
+/**
+ * @brief The ClientConnection class
+ *
+ * This class represents a connection to a server using WebSocket protocol.
+ */
 class ClientConnection : public QObject
 {
     Q_OBJECT
 public:
+    /**
+     * @brief ClientConnection constructor.
+     *
+     * @param url The URL of the server.
+     * @param client_username The username of the client.
+     * @param parent The parent QObject.
+     */
     explicit ClientConnection(QUrl url, QString client_username, QObject *parent = nullptr);
+
+    /**
+     * @brief Starts the connection to the server.
+     */
     void start();
+
+    /**
+     * @brief ClientConnection destructor.
+     */
     ~ClientConnection() override;
 
 protected:
-    QWebSocket m_client;
-    QUrl m_url;
+    QWebSocket m_client; /**< The WebSocket client. */
+    QUrl m_url;          /**< The URL of the server. */
 
     friend class ServerThread;
-
 private:
-    QString client_username;
+    QString client_username; /**< The username of the client. */
+
 signals:
+    /**
+     * @brief Signal emitted when the connection to the server is successful.
+     */
     void connectionSuccess();
+
+    /**
+     * @brief Signal emitted when the connection to the server fails.
+     */
     void connectionFailure();
+
+    /**
+     * @brief Signal emitted when a message is received from the server.
+     *
+     * @param msg The received message.
+     */
     void recievedMessage(Message msg);
+
 public slots:
+    /**
+     * @brief Slot to send a message to the server.
+     *
+     * @param msg The message to send.
+     */
     void sendMessage(QJsonObject msg);
+
 private slots:
+    /**
+     * @brief Slot called when the connection to the server is established.
+     */
     void onConnected();
+
+    /**
+     * @brief Slot called when an error occurs in the connection.
+     *
+     * @param error The error code.
+     */
     void onError(QAbstractSocket::SocketError error);
+
+    /**
+     * @brief Slot called when a text message is received from the server.
+     *
+     * @param message The received message.
+     */
     void onTextMessageReceived(const QString &message);
 };
 
